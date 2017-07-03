6a9d40ab59b6f8af0be84aa99156a46c8ed6f18e
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2017-07-03 11:05:44 +0200

    add requirements

---
+++ requirements.R
@@ -0,0 +1,7 @@
+# install course dependencies
+devtools::install_version("tibble", "1.2")
+devtools::install_version("dplyr", "0.5.0")
+devtools::install_version("data.table", "1.10.0")
+devtools::install_version("hflights", "0.1")
+devtools::install_version("DBI", "0.5-1")
+devtools::install_version("RMySQL", "0.10.9")
